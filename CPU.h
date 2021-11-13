e18eb74eade0ac8d9d2ddd6de106c80428fb1748
ahong75 <ahong75@gatech.edu>
2021-11-12 21:04:10 -0500

    Created CPU header and source file; added basic variables

---
+++ CPU.h
@@ -0,0 +1,22 @@
+#pragma once 
+#include <cstdint>
+
+class Chip8 {
+    public:
+        uint8_t registers[16] {};
+        uint16_t program_counter {};
+        uint16_t index_register {};
+        uint8_t stack_pointer {};
+        uint8_t delay_timer {};
+        uint8_t sound_timer {};
+
+        uint16_t stack[16] {};
+        uint16_t opcode;
+        uint32_t screen[64 * 32] {};
+
+        void fetch();
+
+        void increment_pc();
+    private:
+
+}
