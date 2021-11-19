f546a4142a53de6db017a0196b9ca99c070f0e2c
ahong75 <ahong75@gatech.edu>
2021-11-18 21:34:58 -0500

    Renamed CPU class to Chip8 Created load_rom function and partially completed Chip-8 constructor

--- Chip8.cpp
+++ Chip8.cpp
@@ -1,5 +1,6 @@
-#include "CPU.h"
-#include <bits/stdc++.h>
+#include "Chip8.h"
+#include <fstream>
+#include <iostream>
 
 uint8_t memory[4096] {};
 uint8_t font_sprites[16 * 5] = 
@@ -21,15 +22,48 @@ uint8_t font_sprites[16 * 5] =
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 }
+const unsigned int START_ADD = 0x200;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
 
-CPU::fetch() 
+Chip8::Chip8() 
+{
+    program_counter = START_ADD;
+
+    for(unsigned int i=0; i<FONTSET_SIZE: i++) 
+    {
+        memory[FONTSET_START_ADDRESS + i] = fontset[i];
+    }
+}
+
+Chip8::fetch() 
 {
     opcode = (memory[program_counter]<<8) + memory[program_counter +1];
     increment_pc();
 }
 
-CPU::increment_pc() 
+Chip8::increment_pc() 
 {
     program_counter += 2;
 }
-            
+
+Chip8::load_rom(char const* filename) 
+{
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if(file_is_open())
+    {
+        std::streampos size = file.tellg();
+        char* buffer = new char[size];
+
+
+        file.seekg(0, std::ios::beg);
+        file.read(buffer,size);
+        file.close();
+
+        for(long i = 0; i < size; i++) {
+            memory[START_ADD + i] = buffer[i];
+        }
+
+        delete[] buffer;
+    }
+}
--- Chip8.h
+++ Chip8.h
@@ -3,13 +3,14 @@
 
 class Chip8 {
     public:
+        Chip8();
+    private:
         uint8_t registers[16] {};
         uint16_t program_counter {};
         uint16_t index_register {};
         uint8_t stack_pointer {};
         uint8_t delay_timer {};
         uint8_t sound_timer {};
-
         uint16_t stack[16] {};
         uint16_t opcode;
         uint32_t screen[64 * 32] {};
@@ -17,6 +18,7 @@ class Chip8 {
         void fetch();
 
         void increment_pc();
-    private:
+
+        void load_rom(char const* file);
 
 }
